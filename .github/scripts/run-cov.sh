09c89659fda7c9ce8e3bbd67abd6f4ec435f2811
Tamer Cuba <tamercuba@gmail.com>
2024-05-31 20:37:00 -0300

    misc: fixing cov script

--- .github/scripts/run-cov.sh
+++ .github/scripts/run-cov.sh
@@ -3,11 +3,8 @@
 # Exit immediately if a command exits with a non-zero status.
 set -e
 
-# Run cargo tarpaulin to generate coverage report in XML format
-cargo tarpaulin --lib -p risp_eval --fail-under 50 --out Xml --output-dir target/tarpaulin
-
 # Ensure the target directory exists
 mkdir -p target/tarpaulin
 
-# Move the generated coverage report to the target directory
-mv tarpaulin-report.xml target/tarpaulin/coverage.xml
+# Run cargo tarpaulin to generate coverage report in XML format
+cargo tarpaulin --lib -p risp_eval --fail-under 50 --out Xml --output-dir target/tarpaulin
