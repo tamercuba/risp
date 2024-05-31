ba066ee5925b36a0dae8784400d6b7fb0da2e3c8
Tamer Cuba <tamercuba@gmail.com>
2024-05-31 20:46:29 -0300

    misc: fixing coverage file name

--- .github/scripts/run-cov.sh
+++ .github/scripts/run-cov.sh
@@ -7,7 +7,7 @@ set -e
 mkdir -p target/tarpaulin
 
 # Run cargo tarpaulin to generate coverage report in XML format
-cargo tarpaulin --lib -p risp_eval --fail-under 50 --out Xml --output-dir target/tarpaulin
+cargo tarpaulin --verbose --fail-under 40 --out Xml --output-dir target/tarpaulin
 
 # Checking if coverage file exists 
 ls -la target/tarpaulin
