1da40dd074a7b7e1f0847973a50a5601cd44bf46
Tamer Cuba <tamercuba@gmail.com>
2024-06-03 23:41:42 -0300

    refactor: exposing ch and line number in lexer + improving lexing performance to O(n)

--- risp/src/lexer/implementation.rs
+++ risp/src/lexer/implementation.rs
@@ -1,62 +1,146 @@
 use std::fmt::Display;
 
+#[derive(Debug, Clone)]
+pub struct Content<T> {
+    content: T,
+    ch: usize,
+    line: usize,
+}
+
+impl<T> Display for Content<T> where T: Display {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{line}:{ch} {content}", line = self.line, ch = self.ch, content = self.content)
+    }
+}
+
+impl<T> PartialEq for Content<T> where T: PartialEq {
+    fn eq(&self, other: &Self) -> bool {
+        self.content == other.content
+    }
+}
+
+impl<T> Content<T> {
+    pub fn new(content: T, ch: usize, line: usize) -> Self {
+        Content {
+            content,
+            ch,
+            line,
+        }
+    }
+}
+
 #[derive(Debug, PartialEq, Clone)]
 pub enum Token {
-    Integer(i64),
-    Symbol(String),
-    LParen,
-    RParen,
+    Integer(Content<i64>),
+    Symbol(Content<String>),
+    LParen(Content<()>),
+    RParen(Content<()>),
 }
 
 impl Token {
-    pub fn tokenize(program: &str) -> Result<Vec<Token>, TokenError> {
-        let parsed_program = program.replace("(", " ( ").replace(")", " ) ");
-        let words = parsed_program.split_whitespace();
+    pub fn tokenize(program: &str) -> Vec<Token> {
         let mut tokens: Vec<Token> = vec![];
-        let mut parentheses_count = 0;
-        for word in words {
-            match word {
-                "(" => {
-                    parentheses_count += 1;
-                    tokens.push(Token::LParen);
-                    continue;
+
+        let mut line: usize = 0;
+        let mut ch: usize = 0;
+
+        let mut current_buffer = String::new();
+        let mut chars = program.chars().peekable();
+
+        while let Some(&current) = chars.peek() {
+            match current {
+                '(' => {
+                    tokens.push(Token::LParen(Content::new((), ch, line)));
+                    ch += 1;
+                    chars.next();
                 }
-                ")" => {
-                    parentheses_count -= 1;
-                    tokens.push(Token::RParen);
-                    continue;
+                ')' => {
+                    tokens.push(Token::RParen(Content::new((), ch, line)));
+                    ch += 1;
+                    chars.next();
+                }
+                ' ' => {
+                    let value = current_buffer.clone();
+                    Self::parse_buffer(&value, &mut tokens, &mut ch, &mut line);
+                    current_buffer.clear();
+                    ch += 1;
+                    chars.next();
+                }
+                '\n' => {
+                    let value = current_buffer.clone();
+                    Self::parse_buffer(&value, &mut tokens, &mut ch, &mut line);
+                    current_buffer.clear();
+                    line += 1;
+                    ch = 0;
+                    chars.next();
+                }
+                '\r' => {
+                    if let Some(&'\n') = chars.peek() {
+                        chars.next();
+                    }
+                    let value = current_buffer.clone();
+                    Self::parse_buffer(&value, &mut tokens, &mut ch, &mut line);
+                    current_buffer.clear();
+                    line += 1;
+                    ch = 0;
+                    chars.next();
                 }
                 _ => {
-                    let i = word.parse::<i64>();
-                    if i.is_ok() {
-                        tokens.push(Token::Integer(i.unwrap()));
-                    } else {
-                        tokens.push(Token::Symbol(word.to_string()));
+                    current_buffer.push(current);
+                    if ch == program.len() - 1 {
+                        tokens.push(Token::Symbol(Content::new(current_buffer.clone(), ch, line)));
+                        break;
                     }
                 }
             }
         }
-        if parentheses_count == 0 {
-            Ok(tokens)
-        } else if parentheses_count < 0 {
-            Err(TokenError { ch: ')' })
-        } else {
-            Err(TokenError { ch: '(' })
+        tokens
+    }
+    fn parse_buffer(buffer: &str, tokens: &mut Vec<Token>, ch: &mut usize, line: &mut usize) {
+        match buffer.is_empty() {
+            true => (),
+            false =>
+                match buffer.parse::<i64>() {
+                    Ok(value) => tokens.push(Token::Integer(Content::new(value, *ch, *line))),
+                    Err(_) =>
+                        tokens.push(Token::Symbol(Content::new(buffer.to_string(), *ch, *line))),
+                }
         }
     }
 }
 
-#[derive(Debug)]
-pub struct TokenError {
-    ch: char,
-}
+// #[derive(Debug)]
+// pub struct TokenError {
+//     value: String,
+//     ch: usize,
+//     line: usize,
+// }
 
-impl Display for TokenError {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        match self.ch {
-            '(' => write!(f, "Unmatched closing parenthesis"),
-            ')' => write!(f, "Unmatched opening parenthesis"),
-            _ => write!(f, "Unexpected character: {}", self.ch),
-        }
-    }
-}
+// impl Display for TokenError {
+//     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+//         match self.value.as_str() {
+//             "(" =>
+//                 write!(
+//                     f,
+//                     "{line}:{ch} Unmatched closing parenthesis",
+//                     line = self.line,
+//                     ch = self.ch
+//                 ),
+//             ")" =>
+//                 write!(
+//                     f,
+//                     "{line}:{ch} Unmatched opening parenthesis",
+//                     line = self.line,
+//                     ch = self.ch
+//                 ),
+//             _ =>
+//                 write!(
+//                     f,
+//                     "{line}:{ch} Unexpected character: {value}",
+//                     value = self.value,
+//                     line = self.line,
+//                     ch = self.ch
+//                 ),
+//         }
+//     }
+// }
--- risp/src/lexer/test_lexer.rs
+++ risp/src/lexer/test_lexer.rs
@@ -1,17 +1,17 @@
 #[cfg(test)]
-use super::implementation::Token;
+use super::implementation::{ Token, Content };
 
 #[test]
 fn test_add() {
-    let tokens = Token::tokenize("(+ 1 2)").unwrap_or(vec![]);
+    let tokens = Token::tokenize("(+ 1 2)");
     assert_eq!(
         tokens,
         vec![
-            Token::LParen,
-            Token::Symbol("+".to_string()),
-            Token::Integer(1),
-            Token::Integer(2),
-            Token::RParen
+            Token::LParen(Content::new((), 0, 1)),
+            Token::Symbol(Content::new("+".to_string(), 1, 1)),
+            Token::Integer(Content::new(1, 3, 1)),
+            Token::Integer(Content::new(2, 5, 1)),
+            Token::RParen(Content::new((), 6, 1))
         ]
     )
 }
@@ -25,31 +25,31 @@ fn test_area_of_a_circle() {
      (* pi (* r r))
     )
     ";
-    let tokens = Token::tokenize(program).unwrap_or(vec![]);
+    let tokens = Token::tokenize(program);
     assert_eq!(
         tokens,
         vec![
-            Token::LParen,
-            Token::LParen,
-            Token::Symbol("let".to_string()),
-            Token::Symbol("r".to_string()),
-            Token::Integer(10),
-            Token::RParen,
-            Token::LParen,
-            Token::Symbol("let".to_string()),
-            Token::Symbol("pi".to_string()),
-            Token::Integer(314),
-            Token::RParen,
-            Token::LParen,
-            Token::Symbol("*".to_string()),
-            Token::Symbol("pi".to_string()),
-            Token::LParen,
-            Token::Symbol("*".to_string()),
-            Token::Symbol("r".to_string()),
-            Token::Symbol("r".to_string()),
-            Token::RParen,
-            Token::RParen,
-            Token::RParen
+            Token::LParen(Content::new((), 0, 1)),
+            Token::LParen(Content::new((), 6, 2)),
+            Token::Symbol(Content::new("let".to_string(), 7, 2)),
+            Token::Symbol(Content::new("r".to_string(), 11, 2)),
+            Token::Integer(Content::new(10, 13, 2)),
+            Token::RParen(Content::new((), 15, 2)),
+            Token::LParen(Content::new((), 17, 3)),
+            Token::Symbol(Content::new("let".to_string(), 18, 3)),
+            Token::Symbol(Content::new("pi".to_string(), 22, 3)),
+            Token::Integer(Content::new(314, 25, 3)),
+            Token::RParen(Content::new((), 28, 3)),
+            Token::LParen(Content::new((), 30, 4)),
+            Token::Symbol(Content::new("*".to_string(), 31, 4)),
+            Token::Symbol(Content::new("pi".to_string(), 33, 4)),
+            Token::LParen(Content::new((), 36, 4)),
+            Token::Symbol(Content::new("*".to_string(), 37, 4)),
+            Token::Symbol(Content::new("r".to_string(), 39, 4)),
+            Token::Symbol(Content::new("r".to_string(), 41, 4)),
+            Token::RParen(Content::new((), 42, 4)),
+            Token::RParen(Content::new((), 43, 4)),
+            Token::RParen(Content::new((), 44, 4))
         ]
     );
 }
